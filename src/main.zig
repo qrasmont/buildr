90925e8d6cfd8a1eaa4bc3bd1bd95f454c8d6201
Quentin Rasmont <qrasmont@gmail.com>
2023-04-16 15:52:47 +0200

    Open and read the config content

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
+const print = @import("std").debug.print;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const allocator = std.heap.page_allocator;
+    const flags = std.fs.File.OpenFlags{};
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const file = std.fs.cwd().openFile(".buildr.json", flags) catch |err| {
+        if (err == std.fs.File.OpenError.FileNotFound) {
+            print("No .buildr.json file found in cwd.\n", .{});
+        } else {
+            print("Error on .buildr.json open: {?}.\n", .{err});
+        }
+        return;
+    };
+    defer file.close();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const file_size = try file.getEndPos();
+    const reader = file.reader();
+    const content = try reader.readAllAlloc(allocator, file_size);
+    defer allocator.free(content);
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    print("{s}", .{content});
 }
